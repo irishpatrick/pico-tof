e6bf93d80cd6202f9e3ce39ea83d09e5744037e2
Patrick Roche <rochep63@gmail.com>
2021-06-14 23:14:45 -0400

    latest

--- include/pico_tof.h
+++ include/pico_tof.h
@@ -1,6 +1,14 @@
 #ifndef PICO_TOF
 #define PICO_TOF
 
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
 
+typedef struct _ToF
+{
+    i2c_inst_t* bus;
+} ToF;
+
+int tof_init(ToF*, i2c_inst_t*, uint, uint, uint);
 
 #endif /* PICO_TOF */
